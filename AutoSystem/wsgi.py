f82c718a621e692904d5d0083ba9afe9324c1402
GoTop <imgotop@gmail.com>
2016-03-18 10:39:02 +0800

    在wsgi.py中设置使用的settings文件

--- AutoSystem/wsgi.py
+++ AutoSystem/wsgi.py
@@ -8,7 +8,10 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "AutoSystem.settings")
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                      "AutoSystem.settings.production")
 
 from django.core.wsgi import get_wsgi_application
+
 application = get_wsgi_application()
